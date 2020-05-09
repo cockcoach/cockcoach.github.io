87af236a6fb28c77f6ece4f45c96a1719476db01
Jin <jin.lee@taiger.com>
2020-05-09 22:10:59 +0800

    typing added

---
+++ assets/js/typing.js
@@ -0,0 +1,8 @@
+var typed = new Typed('#typed', {
+    strings: ['I am a Cock','Cock Coach','Ask me anything','seriously^1000','I will give you my advice for a dollar','50% goes to charity'],
+    smartBackspace: true,
+     typeSpeed: 70, // Default value
+
+    backDelay: 700,
+    loop: false
+  });
